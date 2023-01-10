1218a55513b2a1babe7ed2e49deccb8d753bf28c
suraj787 <suraj@bizmap.in>
2023-01-10 08:40:33 +0000

    fields

---
+++ foodbird/foodbird/doctype/party_packages/party_packages.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, bizmap technologies pvt ltd and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Party Packages', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ foodbird/foodbird/doctype/party_packages/test_party_packages.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, bizmap technologies pvt ltd and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestPartyPackages(unittest.TestCase):
+	pass
