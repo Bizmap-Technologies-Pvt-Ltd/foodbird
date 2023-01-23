67f635a32533a334403c106e9485561b1a82f107
RajatBizMap <rajat.singh@bizmap.in>
2023-01-24 01:12:49 +0530

    [party package tbl and attachment mandatory]

--- foodbird/foodbird/doctype/party_packages/party_packages.js
+++ foodbird/foodbird/doctype/party_packages/party_packages.js
@@ -2,7 +2,32 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Party Packages', {
-	// refresh: function(frm) {
+      food_package_template(frm){
+      frappe.call({
+        method: "frappe.client.get_list",    
+        args:{
+           doctype: "Package Template",
+           filters: [
+               ["name",'=',frm.doc.food_package_template]
+            ],
+       fields: ['food']
+      },
+      callback:function(r){
+           cur_frm.clear_table("food_package")
+           cur_frm.refresh_fields("food_package")
+           for (let i = 0; i < r.message.length; i++) {
+                var childTable = cur_frm.add_child("food_package")
+                childTable.food=r.message[i].food
+                cur_frm.refresh_fields("food_package")
+                
+           }
+      } 
+   }) 
+	    
+	}
+	
+})
 
-	// }
-});
+
+
+    
